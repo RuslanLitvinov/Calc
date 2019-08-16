161aa51921b3740da2167b6ae3f05dc814400fa7
Ruslan Litvinov <RuslanLitvinov70@gmail.com>
2019-08-16 17:02:49 +0500

    Рефакторинг: разнесение функционала по центрам ответственности - парсинг строки в отдельный класс, операции и их реализация в отдельный класс.

---
+++ CalcTest/CalcTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcClasses
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void Is_Constant()
+        {
+            var calc = new CalculatorString();
+            string expression = "5";
+
+            string expected = "5";
+            string actual = calc.Calculation(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void OperationPriority()
+        {
+            var calc = new CalculatorString();
+            string expression = "12+3*8/2+3*2/6";
+
+            string expected = "25";
+            string actual = calc.Calculation(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void BracketPriority()
+        {
+            var calc = new CalculatorString();
+            string expression = "12-3*(6-2)/4-7";
+
+            string expected = "2";
+            string actual = calc.Calculation(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void DoubleBracket()
+        {
+            var calc = new CalculatorString();
+            string expression = "7-3*((6-2))/5";
+
+            string expected = "4,6";
+            string actual = calc.Calculation(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ManyBracket()
+        {
+            var calc = new CalculatorString();
+            string expression = "356*((55-(105-50))*3)";
+
+            string expected = "0";
+            string actual = calc.Calculation(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Exception_ExpectedCket()
+        {
+            var calc = new CalculatorString();
+            string expression = "8-2*((6-2)/5";
+
+            string actual = string.Empty;
+            try
+            {
+                actual = calc.Calculation(expression);
+            }
+            catch(System.InvalidOperationException e)
+            {
+                //StringAssert.Contains(e.Message, "Не найдена закрывающая скобка в подвыражении"); 
+                StringAssert.Contains(e.Message, "Не хватает '(' или ')' в выражении");
+                return;            // !!!
+            }
+
+            Assert.Fail("Исключение должно быть, а его нет!");
+
+        }
+        [TestMethod]
+        public void CanNotOperationWithNegativeNumber_NotException()
+        {
+            var calc = new CalculatorString();
+            string expression = "-2+3";
+
+            string expected = "-2+3";
+            string actual = calc.Calculation(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NotExecuteVarible_NotException()
+        {
+            var calc = new CalculatorString();
+            string expression = "2+3+5*6+g";
+
+            string expected = "35+g";
+            string actual = calc.Calculation(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NotExecuteIfUnknownOperationFirst_NotException()
+        {
+            var calc = new CalculatorString();
+            string expression = "g-2-3+5*6";
+
+            string expected = "g-2-3+30";   // Потому, что после умножения первой по приоритету стоит вычитание с 
+                      //неизвестной переменной и от этого зависит дальнейший резальтат. Вычисления нужно остановить.
+            string actual = calc.Calculation(expression);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ OperatorString.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalcClasses
+{
+    /// <summary>
+    /// Реализует функционал математических операция в текстовой строке.
+    /// TODO: реализовать выполенение тригонометрический и прочих самодельных функций так же через метод 
+    ///  ExecuteAction - и представление из через объект action
+    /// </summary>
+    public class OperatorString
+    {
+        /// <summary>
+        /// '+', '-', '*', '/', и др. определяемые разработчиком
+        /// </summary>
+        public List<Operator> operators;
+        public Operator defaultNotFoundOperator;
+        public OperatorString()
+        {
+            this.InitOperators();
+        }
+        public static Operator DefaultNotFoundOperator()
+        {
+            return new Operator() { position = -1, prior = -1 };
+        }
+        public virtual void InitOperators()
+        {
+            this.defaultNotFoundOperator = OperatorString.DefaultNotFoundOperator();
+
+            this.operators = new List<Operator>
+            {
+                new Operator() { type = "*", prior = 0 },
+                new Operator() { type = "/", prior = 0 },
+                new Operator() { type = "+", prior = 1 },
+                new Operator() { type = "-", prior = 1 }
+            };
+            // ...
+        }
+        public string CalcFunction(string funcExpr)
+        {
+            // TODO: по мере необходимости реализовать подсчет различных функций (триг. и др.)
+            return funcExpr;   //    !!!
+        }
+        public string ExecuteAction(Action action)
+        {
+            if (action == null)
+            {
+                return null;              // !!!
+            }
+            if (string.IsNullOrEmpty(action.Type))
+            {
+                return action.Expression;        // !!!
+            }
+
+            action.ParseOperands();
+            decimal? resultDec = null;
+
+            switch (action.Type)
+            {
+                case "*":
+                    if (ValidateTwoOperands(action))
+                    {
+                        resultDec = action.operant1Dec * action.operant2Dec;
+                    }
+                    break;
+                case "/":
+                    if (ValidateTwoOperands(action))
+                    {
+                        resultDec = action.operant1Dec / action.operant2Dec;
+                    }
+
+                    break;
+                case "+":
+                    if (ValidateTwoOperands(action))
+                    {
+                        resultDec = action.operant1Dec + action.operant2Dec;
+                    }
+                    break;
+                case "-":
+                    if (ValidateTwoOperands(action))
+                    {
+                        resultDec = action.operant1Dec - action.operant2Dec;
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException($"Для операции '{action.Type}' не реализовано исполнение.");
+
+            }
+
+            if (resultDec != null)
+            {
+                return resultDec.ToString();       // !!!
+            }
+            else
+            {
+                return action.Expression;         // !!!
+            }
+
+        }
+        private bool ValidateTwoOperands(Action action)
+        {
+            if (action.operant1Dec == null || action.operant2Dec == null)
+            {
+                Console.WriteLine($"Для операции '{action.Type}' нет одного из операндов. Выражение <{action.Expression}> будет результатом.");
+                return false;     // !!!
+            }
+
+            return true;    // !!!
+        }
+
+    }
+}
